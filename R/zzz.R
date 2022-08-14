32831de1346f235db436f8547ebf772ba797d4f6
Obinna Obianom <idonshayo@gmail.com>
2022-08-14 06:20:02 +0000

    version 0.6

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+#' @import shiny
+#' @import utils
+#' @import htmltools
+#' @importFrom jsonlite fromJSON
+#'
+#' @export
+#'
+
+utils::globalVariables(c("updateSelectInput", "updateCheckboxGroupInput", "updateDateInput", "updateDateRangeInput", "updateCheckboxInput", "updateRadioButtons", "updateTextInput", "renderText"))
